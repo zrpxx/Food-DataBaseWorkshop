1afc8791c6e6bed88a2bed9b81fd96dc9b9b3dc6
Eden <zsy010906@gmail.com>
2021-05-26 19:52:54 +0800

    implement spider

---
+++ spider/open_food_facts/open_food_facts/spiders/food_spider.py
@@ -0,0 +1,45 @@
+import scrapy
+
+ # the way to use scrapy css please check "https://blog.csdn.net/dangsh_/article/details/78617178"
+
+class FoodSpider(scrapy.Spider):
+    name = "food"
+
+    def start_requests(self):
+        urls = [
+            'https://world.openfoodfacts.org/2',
+            # https://world.openfoodfacts.org/product/3274080005003/spring-water-cristaline
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        if 'Open Food Facts - World' not in response.css('title::text').get():
+            barcode = response.css("span#barcode::text").get()
+            name = response.css("h1::text").get()
+            brand = response.css("div.row div.medium-12 p a[itemprop]::text").get()
+            category = response.css("div.row div.medium-12 p a[href*='/category']::text").getall()
+            carbohydrate = response.css("tr#nutriment_carbohydrates_tr td.nutriment_value::text").get()
+            fat = response.css("tr#nutriment_fat_tr td.nutriment_value::text").get()
+            sugar = response.css("tr#nutriment_sugars_tr td.nutriment_value::text").get()
+            energy = response.css("tr#nutriment_energy-kcal_tr td.nutriment_value::text").get()
+            protein = response.css("tr#nutriment_proteins_tr td.nutriment_value::text").get()
+            yield {
+                'barcode': barcode,
+                'name': name,
+                'brand': brand,
+                'category': category,
+                'carbohydrate': carbohydrate,
+                'fat': fat,
+                'sugar': sugar,
+                'energy': energy,
+                'protein': protein,
+            }
+
+        else:
+
+            food_pages = response.css("div#search_results li a::attr(href)").getall()
+
+            for food_page in food_pages:
+                food_page = response.urljoin(food_page)
+                yield scrapy.Request(food_page, callback=self.parse)
