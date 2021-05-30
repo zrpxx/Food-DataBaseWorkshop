f465c725878f4a6cfea9e9d15bb91337c936e072
Eden <zsy010906@gmail.com>
2021-05-30 21:21:49 +0800

    implement spider implement login register implement create, delete, update implement search update readme add sql

---
+++ sql/temp_nutri_score.sql
@@ -0,0 +1,5 @@
+INSERT INTO temp.nutri_score (ID, R, DES) VALUES (1, 'A', 'Very good nutritional quality');
+INSERT INTO temp.nutri_score (ID, R, DES) VALUES (2, 'B', 'Good nutritional quality');
+INSERT INTO temp.nutri_score (ID, R, DES) VALUES (3, 'C', 'Average nutritional quality');
+INSERT INTO temp.nutri_score (ID, R, DES) VALUES (4, 'D', 'Poor nutritional quality');
+INSERT INTO temp.nutri_score (ID, R, DES) VALUES (5, 'E', 'Bad nutritional quality');
